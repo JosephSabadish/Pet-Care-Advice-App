80e97a934e86ea3529b1a4d7e86db7bdfc20d637
Joseph Sabadish <j.sabadish@gmail.com>
2018-07-06 12:19:15 -0400

    Add project files.

---
+++ Pet Care Advice App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pet_Care_Advice_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
